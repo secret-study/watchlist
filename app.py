5787dee6064d1dbbe382cb4a11e673cb1fabcec8
secret <1079089556@qq.com>
2020-02-26 01:07:17 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import url_for
 import os
 import sys
 import click
+from flask import request, redirect, flash
 
 from flask_sqlalchemy import SQLAlchemy
 WIN = sys.platform.startswith('win')
@@ -30,11 +31,51 @@ class Movie(db.Model):
 
 name = 'secret'
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid inout.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
     user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',name=name,movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit',movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item update')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>',	methods=['POST'])# 限定只接受POST请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)# 获取电影记录
+    db.session.delete(movie)		# 删除对应的记录
+    db.session.commit()		# 提交数据库会话
+    flash('Item	deleted.')
+    return redirect(url_for('index'))
 
 @ app.route('/home')
 def home():
@@ -83,3 +124,12 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
